b47db4e0d60cd6f02fe28de01b5241b2d5277b04
Roman Chernobrovkin <Roman.Chernobrovkin@myib.com>
2021-03-26 13:36:06 +0200

    fix minor problems.

--- Code/Blazor.AbDatePicker/wwwroot/datepicker-blazor.js
+++ Code/Blazor.AbDatePicker/wwwroot/datepicker-blazor.js
@@ -8,11 +8,32 @@ window.abDatepickerBlazor = {
 
         var $elem = $(elem);
         $elem.datepicker(settings);
+
+        var currDate = null;
         if (date) {
             // set date if initial date is provided
-            var dateObject = new Date(date);
-            $elem.datepicker('setDate', dateObject);
+            currDate = new Date(date);
+            $elem.datepicker('setDate', currDate);
         }
+        $elem.on('change', function () {
+            var oldVal = currDate;
+            var newVal = $elem.datepicker('getDate');
+
+            // solve issues with time zone shift
+            var userTimezoneOffset = oldVal.getTimezoneOffset() * 60000;
+            oldVal = new Date(oldVal.getTime() - userTimezoneOffset);
+
+            userTimezoneOffset = newVal.getTimezoneOffset() * 60000;
+            newVal = new Date(newVal.getTime() - userTimezoneOffset);
+
+            // send data to Blazor component
+            dotnetHelper.invokeMethodAsync('OnChangeAsync',
+                oldVal.toJSON(),
+                newVal.toJSON()
+            );
+
+            currDate = newVal;
+        });
     },
     change: function (elementId, settings) {
         var elem = document.getElementById(elementId);
